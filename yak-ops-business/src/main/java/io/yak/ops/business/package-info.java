2de38a22b62f351a04b797e49add4aaa158cbbe3
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-07-28 13:24:32 +0800

    refactor: remove legacy business package root

--- yak-ops-business/src/main/java/io/yak/ops/business/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Yak Ops product business capabilities organized as vertical domains.
- *
- * <p>Domains remain packages in this module until their size and lifecycle justify independent
- * Maven modules.
- */
-package io.yak.ops.business;
