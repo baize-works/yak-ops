e8fe7d99359d663339b6b80a8269274db82686a4
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-07-28 13:25:13 +0800

    refactor: move resource package to module

--- yak-ops-business/src/main/java/io/yak/ops/business/resource/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Business resource catalog, versions, references and lifecycle capabilities.
- *
- * <p>Physical file storage is provided by {@code yak-framework}'s file abstraction.
- */
-package io.yak.ops.business.resource;
