b85cb5ddf4240dc9f449ed1e552f292b10b552cc
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-07-28 10:22:30 +0800

    refactor: add resource business package

---
+++ yak-ops-business/src/main/java/io/yak/ops/business/resource/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Business resource catalog, versions, references and lifecycle capabilities.
+ *
+ * <p>Physical file storage is provided by {@code yak-framework}'s file abstraction.
+ */
+package io.yak.ops.business.resource;
