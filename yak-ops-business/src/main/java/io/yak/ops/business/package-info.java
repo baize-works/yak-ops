06c3c9e193c85ea38dd4208b4c426b83426d0ab9
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-07-28 10:21:43 +0800

    refactor: define business package boundary

---
+++ yak-ops-business/src/main/java/io/yak/ops/business/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Yak Ops product business capabilities organized as vertical domains.
+ *
+ * <p>Domains remain packages in this module until their size and lifecycle justify independent
+ * Maven modules.
+ */
+package io.yak.ops.business;
