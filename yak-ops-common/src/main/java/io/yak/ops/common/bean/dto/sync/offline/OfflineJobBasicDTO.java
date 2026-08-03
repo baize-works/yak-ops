9bfd5461febd1990c79c34796f98c42123b53307
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-03 11:06:30 +0800

    refactor(offline-api): simplify offline basic contract

--- yak-ops-common/src/main/java/io/yak/ops/common/bean/dto/sync/offline/OfflineJobBasicDTO.java
+++ yak-ops-common/src/main/java/io/yak/ops/common/bean/dto/sync/offline/OfflineJobBasicDTO.java
@@ -1,18 +1,15 @@
 package io.yak.ops.common.bean.dto.sync.offline;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.JsonNode;
-import java.util.LinkedHashMap;
-import java.util.Map;
 import lombok.Data;
 
 /**
- * 离线同步任务基础配置。
+ * 离线同步任务基础信息。
  *
  * @author weifuwan
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class OfflineJobBasicDTO {
@@ -20,19 +17,4 @@ public class OfflineJobBasicDTO {
   private String jobName;
   private String jobDesc;
   private String mode;
-  private String hoconConfig;
-  private String jobDefinitionInfo;
-  private String script;
-
-  private final Map<String, JsonNode> extensions = new LinkedHashMap<>();
-
-  @JsonAnySetter
-  public void putExtension(String name, JsonNode value) {
-    extensions.put(name, value);
-  }
-
-  @JsonAnyGetter
-  public Map<String, JsonNode> getExtensions() {
-    return extensions;
-  }
 }
