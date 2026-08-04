4620253f19a04b3dbc1be144f4effdb94733f65c
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-05 03:19:11 +0800

    feat(workflow): expose V2 workflow versions

--- yak-ops-common/src/main/java/io/yak/ops/common/bean/vo/workflow/WorkflowVersionVO.java
+++ yak-ops-common/src/main/java/io/yak/ops/common/bean/vo/workflow/WorkflowVersionVO.java
@@ -1,6 +1,7 @@
 package io.yak.ops.common.bean.vo.workflow;
 
 import io.yak.ops.common.bean.entity.workflow.WorkflowDag;
+import io.yak.ops.common.bean.entity.workflow.v2.WorkflowV2Dag;
 import java.util.Date;
 import lombok.Data;
 
@@ -10,7 +11,9 @@ public class WorkflowVersionVO {
   private Long id;
   private Long workflowId;
   private int version;
+  private int schemaVersion = 1;
   private WorkflowDag dag;
+  private WorkflowV2Dag dagV2;
   private String contentHash;
   private String publishedBy;
   private Date publishedAt;
