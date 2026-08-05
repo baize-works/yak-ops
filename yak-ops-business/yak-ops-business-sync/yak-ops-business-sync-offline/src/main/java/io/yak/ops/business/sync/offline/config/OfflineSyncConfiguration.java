86ae63c7531597359ec953f396fb5e026467b5dc
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-05 16:57:37 +0800

    refactor(database): reuse shared persistence for offline sync

--- yak-ops-business/yak-ops-business-sync/yak-ops-business-sync-offline/src/main/java/io/yak/ops/business/sync/offline/config/OfflineSyncConfiguration.java
+++ yak-ops-business/yak-ops-business-sync/yak-ops-business-sync-offline/src/main/java/io/yak/ops/business/sync/offline/config/OfflineSyncConfiguration.java
@@ -1,80 +1,34 @@
 package io.yak.ops.business.sync.offline.config;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import io.yak.ops.common.mybatis.MybatisPlusFactorySupport;
+import io.yak.ops.business.datasource.config.BusinessDatabaseConfiguration;
 import java.net.http.HttpClient;
 import javax.sql.DataSource;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.flywaydb.core.Flyway;
 import org.flywaydb.core.api.MigrationVersion;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.PlatformTransactionManager;
 
 /** 离线同步一期基础设施配置。 */
 @ConditionalOnOfflineSyncEnabled
 @Configuration(proxyBeanMethods = false)
 @EnableScheduling
 @EnableConfigurationProperties(OfflineSyncProperties.class)
-@MapperScan(basePackages = "io.yak.ops.business.sync.offline.dao.mapper",
-    sqlSessionFactoryRef = "offlineSyncSqlSessionFactory")
+@Import(BusinessDatabaseConfiguration.class)
+@MapperScan(
+    basePackages = "io.yak.ops.business.sync.offline.dao.mapper",
+    sqlSessionFactoryRef = "yakBusinessSqlSessionFactory")
 public class OfflineSyncConfiguration {
 
-  @Bean(name = "offlineSyncDataSource", destroyMethod = "close")
-  public HikariDataSource offlineSyncDataSource(OfflineSyncProperties properties) {
-    OfflineSyncProperties.Datasource datasource = properties.getDatasource();
-    HikariConfig config = new HikariConfig();
-    config.setPoolName("YakOfflineSyncPool");
-    config.setJdbcUrl(datasource.getUrl());
-    config.setUsername(datasource.getUsername());
-    config.setPassword(datasource.getPassword());
-    config.setDriverClassName(datasource.getDriverClassName());
-    config.setMaximumPoolSize(datasource.getMaximumPoolSize());
-    config.setMinimumIdle(datasource.getMinimumIdle());
-    return new HikariDataSource(config);
-  }
-
-  @Bean(name = "offlineSyncTransactionManager")
-  public PlatformTransactionManager offlineSyncTransactionManager(
-      @Qualifier("offlineSyncDataSource") DataSource dataSource) {
-    return new DataSourceTransactionManager(dataSource);
-  }
-
-  @Bean(name = "offlineSyncSqlSessionFactory")
-  public SqlSessionFactory offlineSyncSqlSessionFactory(
-      @Qualifier("offlineSyncDataSource") DataSource dataSource) throws Exception {
-    MybatisSqlSessionFactoryBean factory = new MybatisSqlSessionFactoryBean();
-    factory.setDataSource(dataSource);
-    factory.setTypeAliasesPackage("io.yak.ops.common.bean.po.sync.offline");
-    factory.setConfiguration(MybatisPlusFactorySupport.createConfiguration());
-    factory.setGlobalConfig(MybatisPlusFactorySupport.createGlobalConfig());
-    MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-    interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-    factory.setPlugins(interceptor);
-    return factory.getObject();
-  }
-
-  @Bean(name = "offlineSyncSqlSessionTemplate")
-  public SqlSessionTemplate offlineSyncSqlSessionTemplate(
-      @Qualifier("offlineSyncSqlSessionFactory") SqlSessionFactory factory) {
-    return new SqlSessionTemplate(factory);
-  }
-
   @Bean(initMethod = "migrate")
-  public Flyway offlineSyncFlyway(@Qualifier("offlineSyncDataSource") DataSource dataSource) {
+  public Flyway offlineSyncFlyway(
+      @Qualifier("yakBusinessDataSource") DataSource dataSource) {
     return Flyway.configure()
         .dataSource(dataSource)
         .locations("classpath:db/migration/yak-offline-sync")
