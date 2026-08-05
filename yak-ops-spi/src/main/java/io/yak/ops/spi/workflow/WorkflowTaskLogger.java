f273e9a9563c8237d4d6236321e3a768648ab90e
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-05 09:46:18 +0800

    refactor: remove legacy workflow and unsupported task modules

--- yak-ops-spi/src/main/java/io/yak/ops/spi/workflow/WorkflowTaskLogger.java
+++
@@ -1,8 +0,0 @@
-package io.yak.ops.spi.workflow;
-
-/** Appends one line to the durable workflow task log. */
-@FunctionalInterface
-public interface WorkflowTaskLogger {
-
-  void log(String line);
-}
