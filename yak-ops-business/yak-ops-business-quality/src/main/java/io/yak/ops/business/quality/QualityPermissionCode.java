a2c913cc7f1cc15b10c640807c954c874296dde7
weifuwan <78582861+weifuwan@users.noreply.github.com>
2026-08-07 10:12:05 +0800

    feat(data-quality): add custom template permissions

--- yak-ops-business/yak-ops-business-quality/src/main/java/io/yak/ops/business/quality/QualityPermissionCode.java
+++ yak-ops-business/yak-ops-business-quality/src/main/java/io/yak/ops/business/quality/QualityPermissionCode.java
@@ -3,6 +3,9 @@ package io.yak.ops.business.quality;
 public final class QualityPermissionCode {
 
   public static final String TEMPLATE_READ = "quality:template:read";
+  public static final String TEMPLATE_CREATE = "quality:template:create";
+  public static final String TEMPLATE_UPDATE = "quality:template:update";
+  public static final String TEMPLATE_DELETE = "quality:template:delete";
   public static final String MONITOR_READ = "quality:monitor:read";
   public static final String MONITOR_CREATE = "quality:monitor:create";
   public static final String MONITOR_UPDATE = "quality:monitor:update";
